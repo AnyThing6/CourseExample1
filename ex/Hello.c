24669c9a500ecfafc23bf3853ee581f2113d027b
Anything <frodi6f@wp.pl>
2019-11-09 15:23:22 +0100

    aaa

--- ex/Hello.c
+++ ex/Hello.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-	printf("Hello from the other side");
+	printf("Hello from the Aha side");
 	int a = 0;
 	printf("Hello from the other side");
 	printf("description in the branch");
-	printf("how the merge tsadol works");
+	printf("how the merge kurwa mergetool works");
+	
 	
 	return 0;
 }
