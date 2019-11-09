8419a70c5fc4e928368f775f64bdd6800a5b6f55
Anything <frodi6f@wp.pl>
2019-11-09 15:12:31 +0100

    clean

--- ex/Hello.c
+++ ex/Hello.c
@@ -2,13 +2,10 @@
 
 int main()
 {
-<<<<<<< HEAD
 	printf("Hello from the other side");
-=======
 	int a = 0;
 	printf("Hello from the other side");
 	printf("description in the branch");
 	
->>>>>>> myBelowedBranch
 	return 0;
 }
